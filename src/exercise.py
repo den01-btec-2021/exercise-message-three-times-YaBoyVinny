ac539a65e0f44596d83829caf6ef641960c554c5
Your Name <you@example.com>
2021-11-26 14:46:43 +0000

    re attempted

--- src/exercise.py
+++ src/exercise.py
@@ -1,13 +1,9 @@
 def message_three_times():
     message = input('Write a message...')
-    for line in range(3):
-        print(message)
+    print(message)
+    print(message)
+    print(message)
 
-
-
-
-
-    
 if __name__ == '__main__':
     message_three_times()
 
